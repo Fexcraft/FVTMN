1cb1376a3cbadba7510a96d2c2c4deda3ad61777
Ferdinand Calo <fercalo96@yahoo.de>
2023-01-11 19:45:38 +0100

    Update uTagMap.java

--- src/net/fexcraft/mod/uni/tag/uTagMap.java
+++ src/net/fexcraft/mod/uni/tag/uTagMap.java
@@ -2,6 +2,43 @@ package net.fexcraft.mod.uni.tag;
 
 import javax.swing.text.html.HTML.Tag;
 
+/**
+ * 
+ * @author Ferdinand Calo' (FEX___96)
+ *
+ */
 public class uTagMap extends Tag {
+
+	public String getString(String string){
+		//
+		return null;
+	}
+
+	public float getFloat(String string){
+		//
+		return 0;
+	}
+
+	public boolean has(String string){
+		//
+		return false;
+	}
+
+	public int getInteger(String string){
+		//
+		return 0;
+	}
+
+	public void set(String key, String value){
+		//
+	}
+
+	public void set(String key, float value){
+		//
+	}
+
+	public void set(String key, int value){
+		//
+	}
 
 }
