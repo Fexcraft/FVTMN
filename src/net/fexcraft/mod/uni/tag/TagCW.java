4ee8d011eb65535b2acbf7bee30dbaf8a2010c4f
Ferdinand Calo <fercalo96@yahoo.de>
2023-06-22 23:06:42 +0200

    Update TagCW.java

--- src/net/fexcraft/mod/uni/tag/TagCW.java
+++ src/net/fexcraft/mod/uni/tag/TagCW.java
@@ -1,5 +1,41 @@
 package net.fexcraft.mod.uni.tag;
 
-public class TagCW {
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public interface TagCW {
+
+	public String getString(String key);
+
+	public float getFloat(String key);
+
+	public int getInteger(String key);
+
+	public boolean getBoolean(String key);
+
+	public boolean has(String key);
+
+	public void set(String key, String val);
+
+	public void set(String key, float val);
+
+	public void set(String key, int val);
+
+	public void set(String key, boolean val);
+
+	//
+
+	public static Class<? extends TagCW>[] IMPL = new Class[1];
+
+	public static TagCW create(){
+		try {
+			return IMPL[0].newInstance();
+		}
+		catch (InstantiationException | IllegalAccessException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public <C> C cast();
 
 }
