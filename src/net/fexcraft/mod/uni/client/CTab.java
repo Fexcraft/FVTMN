82574d9a4a18a880e6d78de91a24c3816c19b0a0
Ferdinand Calo <fercalo96@yahoo.de>
2023-06-28 10:29:12 +0200

    Update CTab.java

--- src/net/fexcraft/mod/uni/client/CTab.java
+++ src/net/fexcraft/mod/uni/client/CTab.java
@@ -3,7 +3,7 @@ package net.fexcraft.mod.uni.client;
 import java.lang.reflect.InvocationTargetException;
 import java.util.TreeMap;
 
-import net.fexcraft.mod.fvtm.data.addon.AddonNew;
+import net.fexcraft.mod.fvtm.data.addon.Addon;
 import net.fexcraft.mod.uni.IDL;
 import net.fexcraft.mod.uni.IDLManager;
 
@@ -16,11 +16,11 @@ public interface CTab {
 	public static final String DEFAULT = "default";
 	public static Class<? extends CTab>[] IMPL = new Class[1];
 
-	public static CTab create(AddonNew addon, String id){
+	public static CTab create(Addon addon, String id){
 		IDL aid = IDLManager.getIDLCached(addon.getID().id() + ":" + id);
 		if(TABS.containsKey(aid)) return TABS.get(aid);
 		try {
-			TABS.put(aid, IMPL[0].getConstructor(AddonNew.class, String.class).newInstance(addon, id));
+			TABS.put(aid, IMPL[0].getConstructor(Addon.class, String.class).newInstance(addon, id));
 		}
 		catch (InstantiationException | NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
 			throw new RuntimeException(e);
