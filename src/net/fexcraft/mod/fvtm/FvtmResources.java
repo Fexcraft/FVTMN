ebee478846352b621cc658b7ae97a443b57d34f1
Ferdinand Calo <fercalo96@yahoo.de>
2023-06-29 22:42:15 +0200

    Update FvtmResources.java

--- src/net/fexcraft/mod/fvtm/FvtmResources.java
+++ src/net/fexcraft/mod/fvtm/FvtmResources.java
@@ -3,11 +3,20 @@ package net.fexcraft.mod.fvtm;
 import static net.fexcraft.mod.fvtm.FvtmRegistry.ADDONS;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+import java.util.zip.ZipInputStream;
 
 import net.fexcraft.app.json.JsonArray;
 import net.fexcraft.app.json.JsonHandler;
 import net.fexcraft.app.json.JsonMap;
 import net.fexcraft.app.json.JsonValue;
+import net.fexcraft.lib.mc.utils.Print;
+import net.fexcraft.lib.mc.utils.Static;
+import net.fexcraft.mod.fvtm.data.Content;
+import net.fexcraft.mod.fvtm.data.ContentType;
 import net.fexcraft.mod.fvtm.data.addon.Addon;
 import net.fexcraft.mod.fvtm.data.addon.AddonLocation;
 import net.fexcraft.mod.fvtm.util.ContentConfigUtil;
@@ -82,5 +91,76 @@ public abstract class FvtmResources {
 	}
 
 	public abstract void loadPackTextures();
+
+	public void searchInPacksFor(ContentType contype){
+		if(contype == ContentType.ADDON) return;
+		for(Addon addon : ADDONS){
+			if(addon.getFile().isDirectory()){
+				File folder = new File(addon.getFile(), "assets/" + addon.getID().id() + "/config/" + contype.folder + "/");
+				if(!folder.exists()) continue;
+				ArrayList<File> files = findFiles(folder, contype.suffix);
+				for(File file : files){
+					try{
+						JsonMap map = JsonHandler.parse(file);
+						Content<?> content = (Content<?>)contype.impl.newInstance().parse(map);
+						if(content == null){
+							IDL idl = ContentConfigUtil.getID(map);
+							Print.log("Errors while loading config file: " + file + " for " + idl.colon()); Static.stop();
+						}
+						contype.register(content);
+						if(EnvInfo.CLIENT) checkForCustomModel(addon.getLocation(), contype, content);
+					}
+					catch(Exception e){
+						Print.log("Errors while loading config file: " + file); Static.stop();
+					}
+				}
+			}
+			else{
+				String lastentry = null;
+				try{
+					String path = "assets/" + addon.getID().id() + "/config/" + contype.folder + "/";
+					ZipFile zip = new ZipFile(addon.getFile());
+					ZipInputStream stream = new ZipInputStream(new FileInputStream(addon.getFile()));
+					ZipEntry entry = null;
+					while(true){
+						entry = stream.getNextEntry();
+						if(entry == null) break;
+						lastentry = entry.getName();
+						if(entry.getName().startsWith(path) && entry.getName().endsWith(contype.suffix)){
+							JsonMap map = JsonHandler.parse(zip.getInputStream(entry));
+							Content<?> content = (Content<?>)contype.impl.newInstance().parse(map);
+							if(content == null){
+								IDL idl = ContentConfigUtil.getID(map);
+								Print.log("Errors while loading config from zip: " + addon.getFile() + " for " + idl.colon()); Static.stop();
+							}
+							contype.register(content);
+							if(EnvInfo.CLIENT) checkForCustomModel(addon.getLocation(), contype, content);
+						}
+					}
+				}
+				catch (Exception e){
+					Print.log("Errors while loading config from zip: " + addon.getFile() + " - " + lastentry); Static.stop();
+				}
+			}
+		}
+	}
+
+	public static ArrayList<File> findFiles(File file, String suffix){
+		ArrayList<File> result = new ArrayList<>();
+		if(file.isDirectory()){
+			for(File sub : file.listFiles()){
+				ArrayList<File> search = findFiles(sub, suffix);
+				if(!search.isEmpty()) result.addAll(search);
+			}
+		}
+		else if(file.getName().endsWith(suffix)) result.add(file);
+		return result;
+	}
+
+	public abstract void checkForCustomModel(AddonLocation loc, ContentType contype, Content<?> content);
+
+	public static void loadRecipes(){
+
+	}
 
 }
