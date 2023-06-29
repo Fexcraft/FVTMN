0025200a7dac026ae235789fde72fe167e226c78
Ferdinand Calo <fercalo96@yahoo.de>
2023-06-29 23:11:28 +0200

    Update FvtmRegistry.java

--- src/net/fexcraft/mod/fvtm/FvtmRegistry.java
+++ src/net/fexcraft/mod/fvtm/FvtmRegistry.java
@@ -3,9 +3,11 @@ package net.fexcraft.mod.fvtm;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.TreeMap;
 
 import net.fexcraft.app.json.JsonMap;
 import net.fexcraft.mod.fvtm.data.DecorationData;
+import net.fexcraft.mod.fvtm.data.Fuel;
 import net.fexcraft.mod.fvtm.data.addon.Addon;
 import net.fexcraft.mod.fvtm.sys.particle.Particle;
 import net.fexcraft.mod.fvtm.util.Registry;
@@ -23,11 +25,14 @@ public class FvtmRegistry {
 	public static String LOADER_VER;
 	public static File CONFIG_DIR;
 	//
+	public static IDL INTERNAL_ADDON_ID = IDLManager.getIDLCached("fvtm:fvtm");
 	public static IDL NONE_CLOTH_MAT;
 	public static final IDL NULL_TEXTURE = IDLManager.getIDLNamed("No Texture;fvtm:textures/entity/null.png");
 	public static final IDL WHITE_TEXTURE = IDLManager.getIDLNamed("No Texture;fvtm:textures/entity/white.png");
 	//
 	public static final Registry<Addon> ADDONS = new Registry<>();
+	public static final Registry<Fuel> FUELS = new Registry<>();
+	public static TreeMap<String, TreeMap<String, ArrayList<Fuel>>> SORTED_FUELS = new TreeMap<>();
 	public static final HashMap<String, Particle> PARTICLES = new HashMap<>();
 	public static final HashMap<String, JsonMap> WIRE_DECO_CACHE = new HashMap<>();
 	public static final HashMap<String, DecorationData> DECORATIONS = new HashMap<>();
@@ -38,6 +43,21 @@ public class FvtmRegistry {
 		CONFIG_DIR = conf;
 		if(!CONFIG_DIR.exists()) CONFIG_DIR.mkdirs();
 		Config.init(new File(conf.getParentFile(), "fvtm.json"));
+	}
+
+	public static Addon getAddon(String id){
+		for(Addon addon : ADDONS){
+			if(addon.getID().id().equals(id)) return addon;
+		}
+		return null;
+	}
+
+	public static Fuel getFuel(String id){
+		return FUELS.get(id);
+	}
+
+	public static Fuel getFuel(IDL idl){
+		return FUELS.get(idl);
 	}
 
 }
