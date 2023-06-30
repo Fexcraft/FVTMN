8fa10e12b5d692d3b3da9b778885acc394aa3830
Ferdinand Calo <fercalo96@yahoo.de>
2023-06-30 23:57:38 +0200

    Update StackWrapper.java

--- src/net/fexcraft/mod/uni/item/StackWrapper.java
+++ src/net/fexcraft/mod/uni/item/StackWrapper.java
@@ -1,7 +1,15 @@
 package net.fexcraft.mod.uni.item;
 
-public interface StackWrapper {
+public class StackWrapper {
 
 	public static StackWrapper EMPTY = null;
+
+	public StackWrapper(ItemWrapper item){
+
+	}
+
+	public <IS> IS local(){
+		return null;
+	}
 
 }
