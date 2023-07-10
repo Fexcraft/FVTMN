964bdd6b0fe96fa4c893209814e3f3d3a739a774
Ferdinand Calo <fercalo96@yahoo.de>
2023-07-10 22:09:25 +0200

    Update ContentItem.java

--- src/net/fexcraft/mod/fvtm/data/ContentItem.java
+++ src/net/fexcraft/mod/fvtm/data/ContentItem.java
@@ -1,23 +1,27 @@
 package net.fexcraft.mod.fvtm.data;
 
-import javax.annotation.Nullable;
-
 import net.fexcraft.mod.uni.item.StackWrapper;
 import net.fexcraft.mod.uni.tag.TagCW;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
-public interface ContentItem<TYPE extends Content<TYPE>, DATA> {
+public interface ContentItem<TYPE extends Content<TYPE>> {
 
 	public TYPE getContent();
 
 	public ContentType getType();
 
-	@Nullable
-	public ContentData<TYPE, DATA> getData(StackWrapper stack);
+	public interface ContentDataItem<TYPE extends Content<TYPE>, DATA extends ContentData<TYPE, DATA>> extends ContentItem<TYPE> {
 
-	@Nullable
-	public ContentData<TYPE, DATA> getData(TagCW compound);
+		public default DATA getData(StackWrapper stack){
+			return null;
+		}
+
+		public default DATA getData(TagCW compound){
+			return null;
+		}
+
+	}
 
 }
