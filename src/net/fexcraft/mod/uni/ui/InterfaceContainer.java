fc47d58da803d0d4fe84dcdf289af32858ab7f0e
Ferdinand Calo <fercalo96@yahoo.de>
2023-07-16 23:19:58 +0200

    Create InterfaceContainer.java

---
+++ src/net/fexcraft/mod/uni/ui/InterfaceContainer.java
@@ -0,0 +1,18 @@
+package net.fexcraft.mod.uni.ui;
+
+import net.fexcraft.app.json.JsonMap;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class InterfaceContainer {
+
+	public static Class<InterfaceContainer> IMPLEMENTATION;
+	//
+
+
+	public InterfaceContainer(JsonMap map){
+
+	}
+
+}
