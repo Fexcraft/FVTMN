a82aa520d074c890139657cf8d1285a876a3c7da
Ferdinand Calo <fercalo96@yahoo.de>
2023-07-21 20:03:24 +0200

    Update UserInterface.java

--- src/net/fexcraft/mod/uni/ui/UserInterface.java
+++ src/net/fexcraft/mod/uni/ui/UserInterface.java
@@ -3,6 +3,7 @@ package net.fexcraft.mod.uni.ui;
 import java.util.LinkedHashMap;
 import java.util.Map.Entry;
 
+import net.fexcraft.app.json.JsonArray;
 import net.fexcraft.app.json.JsonMap;
 import net.fexcraft.app.json.JsonValue;
 
@@ -13,13 +14,15 @@ public abstract class UserInterface {
 
 	public static Class<? extends UserInterface> IMPLEMENTATION;
 	//
-	public InterfaceContainer container;
+	public ContainerInterface container;
 	public LinkedHashMap<String, UIText> texts = new LinkedHashMap<>();
 	public LinkedHashMap<String, UIButton> buttons = new LinkedHashMap<>();
 	public LinkedHashMap<String, UIField> fields = new LinkedHashMap<>();
 	public LinkedHashMap<String, UITab> tabs = new LinkedHashMap<>();
+	public boolean background;
+	public int width, height;
 
-	public UserInterface(JsonMap map, InterfaceContainer container) throws Exception {
+	public UserInterface(JsonMap map, ContainerInterface container) throws Exception {
 		this.container = container;
 		if(map.has("texts")){
 			for(Entry<String, JsonValue<?>> entry : map.getMap("texts").entries()){
@@ -48,6 +51,10 @@ public abstract class UserInterface {
 			main.fields.addAll(fields.values());
 			tabs.put("main", main);
 		}
+		background = map.getBoolean("background", true);
+		JsonArray arr = map.getArray("size");
+		width = arr.get(0).integer_value();
+		height = arr.get(1).integer_value();
 	}
 
 	public boolean onClick(int mx, int my, int mb){
