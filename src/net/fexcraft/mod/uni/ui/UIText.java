c5fe2cf60e99c8b2cf0c87d37dfad236a721935d
Ferdinand Calo <fercalo96@yahoo.de>
2023-07-22 23:46:22 +0200

    Update UIText.java

--- src/net/fexcraft/mod/uni/ui/UIText.java
+++ src/net/fexcraft/mod/uni/ui/UIText.java
@@ -1,6 +1,7 @@
 package net.fexcraft.mod.uni.ui;
 
 import net.fexcraft.app.json.JsonMap;
+import net.fexcraft.lib.common.math.RGB;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
@@ -12,8 +13,10 @@ public class UIText extends UIElement {
 	public String initial_value;
 	public String value;
 	public boolean shadow;
+	public boolean translate;
 	public float scale;
-	public int color;
+	public RGB color = new RGB();
+	public RGB hover = new RGB();
 
 	public UIText(UserInterface ui, JsonMap map) throws Exception {
 		super(ui, map);
@@ -21,7 +24,16 @@ public class UIText extends UIElement {
 		scale = map.getFloat("scale", 1);
 		if(map.getBoolean("autoscale", false)) scale = -1;
 		shadow = map.getBoolean("shadow", false);
-		color = map.getInteger("color", 0xf0f0f0);
+		color.packed = map.getInteger("color", 0xf0f0f0);
+		if(map.getBoolean("hoverable", true)){
+			hover.packed = map.getInteger("hover", 0xf4d742);
+		}
+		translate = map.getBoolean("translate", false);
+		if(translate) translate();
 	}
+
+	public void translate(){}
+
+	public void translate(Object... objects){}
 
 }
