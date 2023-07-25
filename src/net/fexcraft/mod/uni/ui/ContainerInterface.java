45e397d9468354d194ef8f2aac311f4ff1479728
Ferdinand Calo <fercalo96@yahoo.de>
2023-07-25 23:07:28 +0200

    Update ContainerInterface.java

--- src/net/fexcraft/mod/uni/ui/ContainerInterface.java
+++ src/net/fexcraft/mod/uni/ui/ContainerInterface.java
@@ -5,10 +5,12 @@ import net.fexcraft.app.json.JsonMap;
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
-public class ContainerInterface {
+public abstract class ContainerInterface {
 
 	public ContainerInterface(JsonMap map){
 
 	}
+
+	public abstract Object get(String key, Object... objs);
 
 }
