57ecb2feb2d4a3661b6b19a0f6287d254b434fdc
Ferdinand Calo <fercalo96@yahoo.de>
2023-07-27 17:36:14 +0200

    Update ContainerInterface.java

--- src/net/fexcraft/mod/uni/ui/ContainerInterface.java
+++ src/net/fexcraft/mod/uni/ui/ContainerInterface.java
@@ -1,16 +1,30 @@
 package net.fexcraft.mod.uni.ui;
 
+import java.util.function.Consumer;
+
 import net.fexcraft.app.json.JsonMap;
+import net.fexcraft.mod.uni.tag.TagCW;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
 public abstract class ContainerInterface {
 
+	public Consumer<TagCW> SEND_TO_CLIENT;
+	public Consumer<TagCW> SEND_TO_SERVER;
+	protected UserInterface ui;//client side only
+
 	public ContainerInterface(JsonMap map){
 
 	}
 
 	public abstract Object get(String key, Object... objs);
+
+	public abstract void packet(TagCW com, boolean client);
+
+	public ContainerInterface set(UserInterface ui){
+		this.ui = ui;
+		return this;
+	}
 
 }
