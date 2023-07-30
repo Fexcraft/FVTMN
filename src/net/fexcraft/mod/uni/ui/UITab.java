b9e24025192e00a932dd75fc6f9449fdb9ee7baf
Ferdinand Calo <fercalo96@yahoo.de>
2023-07-30 22:31:07 +0200

    Update UITab.java

--- src/net/fexcraft/mod/uni/ui/UITab.java
+++ src/net/fexcraft/mod/uni/ui/UITab.java
@@ -1,6 +1,6 @@
 package net.fexcraft.mod.uni.ui;
 
-import java.util.ArrayList;
+import java.util.LinkedHashMap;
 
 import net.fexcraft.app.json.JsonMap;
 
@@ -11,25 +11,25 @@ public abstract class UITab extends UIButton {
 
 	public static Class<? extends UITab> IMPLEMENTATION;
 	//
-	public ArrayList<UIText> texts = new ArrayList<>();
-	public ArrayList<UIButton> buttons = new ArrayList<>();
-	public ArrayList<UIField> fields = new ArrayList<>();
+	public LinkedHashMap<String, UIText> texts = new LinkedHashMap<>();
+	public LinkedHashMap<String, UIButton> buttons = new LinkedHashMap<>();
+	public LinkedHashMap<String, UIField> fields = new LinkedHashMap<>();
 
 	public UITab(UserInterface ui, JsonMap map) throws Exception {
 		super(ui, map);
 		if(map.has("buttons")){
 			map.getArray("buttons").value.forEach(json -> {
-				buttons.add(ui.buttons.get(json.string_value()));
+				buttons.put(json.string_value(), ui.buttons.get(json.string_value()));
 			});
 		}
 		if(map.has("texts")){
 			map.getArray("texts").value.forEach(json -> {
-				texts.add(ui.texts.get(json.string_value()));
+				texts.put(json.string_value(), ui.texts.get(json.string_value()));
 			});
 		}
 		if(map.has("fields")){
 			map.getArray("fields").value.forEach(json -> {
-				fields.add(ui.fields.get(json.string_value()));
+				fields.put(json.string_value(), ui.fields.get(json.string_value()));
 			});
 		}
 	}
