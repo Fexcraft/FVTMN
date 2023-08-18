bff017a74bf91ee2e2887034f0df8a025a7731c3
Ferdinand Calo <fercalo96@yahoo.de>
2023-08-18 23:00:37 +0200

    Create Vehicle2.java

---
+++ src/net/fexcraft/mod/fvtm/data/vehicle/Vehicle2.java
@@ -0,0 +1,136 @@
+package net.fexcraft.mod.fvtm.data.vehicle;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import net.fexcraft.app.json.JsonMap;
+import net.fexcraft.app.json.JsonValue;
+import net.fexcraft.lib.common.math.RGB;
+import net.fexcraft.lib.common.math.V3D;
+import net.fexcraft.mod.fvtm.data.Content;
+import net.fexcraft.mod.fvtm.data.ContentType;
+import net.fexcraft.mod.fvtm.data.SwivelPoint;
+import net.fexcraft.mod.fvtm.data.attribute.Attribute;
+import net.fexcraft.mod.fvtm.data.part.PartSlot.PartSlots;
+import net.fexcraft.mod.fvtm.data.root.ItemTextureable;
+import net.fexcraft.mod.fvtm.data.root.WithItem;
+import net.fexcraft.mod.fvtm.model.Model;
+import net.fexcraft.mod.fvtm.model.ModelData;
+import net.fexcraft.mod.fvtm.util.ContentConfigUtil;
+import net.fexcraft.mod.uni.IDL;
+import net.fexcraft.mod.uni.IDLManager;
+import net.fexcraft.mod.uni.item.ItemWrapper;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class Vehicle2 extends Content<Vehicle2> implements WithItem, ItemTextureable {//texture, color, sound
+
+	protected Map<String, Attribute<?>> attributes = new LinkedHashMap<>();
+	protected Map<String, WheelSlot2> wheelpos = new LinkedHashMap<>();
+	protected Model model;
+	protected ModelData modeldata;
+	protected List<IDL> textures;
+	protected List<String> categories;
+	protected Map<String, RGB> channels = new LinkedHashMap<>();
+	protected String modelid;
+	protected String ctab;
+	protected boolean trailer;
+	protected V3D conn_front;
+	protected V3D conn_rear;
+	protected Map<String, IDL> installed = new LinkedHashMap<>();
+	protected Map<String, Object> sounds = new LinkedHashMap<>();
+	protected Map<String, SwivelPoint> swivelpoints = new LinkedHashMap<>();
+	protected float coupler_range;
+	protected Map<String, LiftingPoint> liftingpoints = new HashMap<>();
+	protected IDL keytype;
+	protected int maxkeys;
+	protected PartSlots partslots;
+	protected ItemWrapper item;
+	protected IDL itemtexloc;
+	protected boolean no3ditem;
+	protected VehicleType type;
+
+	public Vehicle2(){}
+
+	@Override
+	public Vehicle2 parse(JsonMap map){
+		if((pack = ContentConfigUtil.getAddon(map)) == null) return null;
+		if((id = ContentConfigUtil.getID(pack, map)) == null) return null;
+		//
+		name = map.getString("Name", "Unnamed Material");
+		description = ContentConfigUtil.getStringList(map, "Description");
+		type = VehicleType.valueOf(map.getString("VehicleType", "LAND").toUpperCase());
+		textures = ContentConfigUtil.getTextures(map);
+		if(map.has("ColorChannels")){
+			for(Entry<String, JsonValue<?>> entry : map.get("ColorChannels").asMap().entries()){
+				channels.put(entry.getKey(), new RGB(entry.getValue().string_value()));
+			}
+		}
+		if(channels.isEmpty()){
+			channels.put("primary", RGB.WHITE.copy());
+			channels.put("secondary", RGB.WHITE.copy());
+		}
+		categories = map.has("Categories") ? map.getArray("Categories").toStringList() : new ArrayList<>();
+		if(map.has("Category")){
+			String cat = map.get("Category").string_value();
+			if(cat.contains(",")){
+				String[] cats = cat.split(",");
+				for(String str : cats) categories.add(str.trim());
+			}
+			else categories.add(cat);
+		}
+		maxkeys = map.getInteger("MaxKeys", 5);
+		keytype = map.has("KeyType") ? IDLManager.getIDLCached(map.getString("KeyType", null)) : null;
+		if(map.has("Attributes")){
+			for(Entry<String, JsonValue<?>> entry : map.getMap("Attributes").entries()){
+				Attribute<?> attr = Attribute.parse(entry.getKey(), entry.getValue().asMap());
+				if(attr != null) attributes.put(attr.id, attr);
+			}
+		}
+		List<Attribute<?>> attrs = type.getDefaultAttributesForType(trailer);
+		for(Attribute<?> attr : attrs){
+			if(!attributes.containsKey(attr.id)){
+				attributes.put(attr.id, attr);
+			}
+			else{
+				Attribute<?> attri = attributes.get(attr.id);
+				attri.limit(attr.min, attr.max);
+				attri.group(attr.group);
+				attri.sync(attr.sync);
+				attri.icons.putAll(attr.icons);
+			}
+		}
+		//
+		return this;
+	}
+
+	@Override
+	public ContentType getContentType(){
+		return ContentType.VEHICLE;
+	}
+
+	@Override
+	public Class<?> getDataClass(){
+		return null;//TODO
+	}
+
+	@Override
+	public String getItemContainer(){
+		return null;
+	}
+
+	@Override
+	public String getCreativeTab(){
+		return ctab;
+	}
+
+	@Override
+	public IDL getItemTexture(){
+		return itemtexloc;
+	}
+}
