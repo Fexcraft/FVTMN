8f8d1e71c96f125a4429e6f63cafc27c4267c2f8
Ferdinand Calo <fercalo96@yahoo.de>
2023-08-18 23:57:54 +0200

    Update Vehicle2.java

--- src/net/fexcraft/mod/fvtm/data/vehicle/Vehicle2.java
+++ src/net/fexcraft/mod/fvtm/data/vehicle/Vehicle2.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
+import net.fexcraft.app.json.JsonArray;
 import net.fexcraft.app.json.JsonMap;
 import net.fexcraft.app.json.JsonValue;
 import net.fexcraft.lib.common.math.RGB;
@@ -21,6 +22,7 @@ import net.fexcraft.mod.fvtm.data.root.WithItem;
 import net.fexcraft.mod.fvtm.model.Model;
 import net.fexcraft.mod.fvtm.model.ModelData;
 import net.fexcraft.mod.fvtm.util.ContentConfigUtil;
+import net.fexcraft.mod.uni.EnvInfo;
 import net.fexcraft.mod.uni.IDL;
 import net.fexcraft.mod.uni.IDLManager;
 import net.fexcraft.mod.uni.item.ItemWrapper;
@@ -31,7 +33,7 @@ import net.fexcraft.mod.uni.item.ItemWrapper;
 public class Vehicle2 extends Content<Vehicle2> implements WithItem, ItemTextureable {//texture, color, sound
 
 	protected Map<String, Attribute<?>> attributes = new LinkedHashMap<>();
-	protected Map<String, WheelSlot2> wheelpos = new LinkedHashMap<>();
+	protected Map<String, WheelSlot> wheelpos = new LinkedHashMap<>();
 	protected Model model;
 	protected ModelData modeldata;
 	protected List<IDL> textures;
@@ -39,13 +41,14 @@ public class Vehicle2 extends Content<Vehicle2> implements WithItem, ItemTexture
 	protected Map<String, RGB> channels = new LinkedHashMap<>();
 	protected String modelid;
 	protected String ctab;
+	protected SimplePhysData sphdata;
 	protected boolean trailer;
 	protected V3D conn_front;
 	protected V3D conn_rear;
-	protected Map<String, IDL> installed = new LinkedHashMap<>();
+	protected Map<String, IDL> installed;
 	protected Map<String, Object> sounds = new LinkedHashMap<>();
 	protected Map<String, SwivelPoint> swivelpoints = new LinkedHashMap<>();
-	protected float coupler_range;
+	protected float coupler_range = 1f;
 	protected Map<String, LiftingPoint> liftingpoints = new HashMap<>();
 	protected IDL keytype;
 	protected int maxkeys;
@@ -105,7 +108,45 @@ public class Vehicle2 extends Content<Vehicle2> implements WithItem, ItemTexture
 				attri.icons.putAll(attr.icons);
 			}
 		}
-		//
+		if(map.has("WheelPositions")){
+			for(Entry<String, JsonValue<?>> entry : map.get("WheelPositions").asMap().entries()){
+				wheelpos.put(entry.getKey(), new WheelSlot(entry.getValue().asMap()));
+			}
+		}
+		if(map.has("SimplePhysics")){
+			sphdata = new SimplePhysData(map.getMap("SimplePhysics"));
+		}
+		trailer = map.getBoolean("Trailer", false) || map.getBoolean("Wagon", false);
+		if(map.has("ConnectorFront")){
+			conn_front = ContentConfigUtil.getVector(map.getArray("ConnectorFront"));
+		}
+		if(map.has("ConnectorRear")){
+			conn_rear = ContentConfigUtil.getVector(map.getArray("ConnectorRear"));
+		}
+		coupler_range = map.getFloat("CouplerRange", coupler_range);
+		if(map.has("InstalledParts")){
+			installed = new LinkedHashMap<>();
+			for(Entry<String, JsonValue<?>> entry : map.getMap("InstalledParts").entries()){
+				installed.put(entry.getKey(), IDLManager.getIDLCached(entry.getValue().string_value()));
+			}
+		}
+		if(map.has("SwivelPoints")){
+			//
+		}
+		if(map.has("Sounds")){
+			//
+		}
+		if(map.has("LiftingPoints")){
+			//
+		}
+		partslots = new PartSlots("vehicle", map.has("PartSlots") ? map.getArray("PartSlots") : new JsonArray());
+		if(EnvInfo.CLIENT){
+			modelid = map.getString("Model", null);
+			modeldata = ContentConfigUtil.getModelData(map, "ModelData", new ModelData());
+		}
+		ctab = map.getString("CreativeTab", "default");
+		itemtexloc = ContentConfigUtil.getItemTexture(id, getContentType(), map);
+		no3ditem = map.getBoolean("Disable3DItemModel", false);
 		return this;
 	}
 
