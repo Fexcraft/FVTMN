8969973da161466343e254658019b6ce36856d67
Ferdinand Calo <fercalo96@yahoo.de>
2023-08-20 19:34:20 +0200

    Create EntityW.java

---
+++ src/net/fexcraft/mod/uni/world/EntityW.java
@@ -0,0 +1,12 @@
+package net.fexcraft.mod.uni.world;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public abstract class EntityW {
+
+	public abstract boolean isOnClient();
+
+	public abstract int getId();
+
+}
