a6748b905f6900966a46e9dff4cc29f00186b88d
Ferdinand Calo <fercalo96@yahoo.de>
2023-09-04 22:27:12 +0200

    Create PartFunction.java

---
+++ src/net/fexcraft/mod/fvtm/data/part/PartFunction.java
@@ -0,0 +1,53 @@
+package net.fexcraft.mod.fvtm.data.part;
+
+import java.util.List;
+
+import net.fexcraft.app.json.FJson;
+import net.fexcraft.mod.uni.item.StackWrapper;
+import net.fexcraft.mod.uni.tag.TagCW;
+import net.fexcraft.mod.uni.world.WorldW;
+
+/**
+ * Part Function
+ *
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public abstract class PartFunction {
+
+	public abstract PartFunction init(Part2 part, FJson json);
+	
+	public abstract PartFunction load(TagCW compound);
+	
+	public abstract TagCW save(TagCW compound);
+
+	public abstract String getId();
+	
+	/** Used to crease an use-copy from the "default" function stored in a Part. */
+	public abstract PartFunction copy(Part2 Part);
+	
+	public static abstract class StaticFunction extends PartFunction {
+
+		@Override
+		public PartFunction load(TagCW compound){
+			return this;
+		}
+
+		@Override
+		public TagCW save(TagCW compound){
+			return null;
+		}
+
+		@Override
+		public PartFunction copy(Part2 part){
+			return this;
+		}
+		
+	}
+
+    public void addInformation(StackWrapper stack, WorldW world, PartData data, List<String> list, boolean extended){}
+
+	public boolean onCondition(String[] targets, String mode, String condi){
+		return false;
+	}
+
+}
