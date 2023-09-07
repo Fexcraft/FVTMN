ee4c23d3dc69f2cb6e321dc855cf5cd346ce2340
Ferdinand Calo <fercalo96@yahoo.de>
2023-09-07 20:40:18 +0200

    Create PartInstallHandler.java

---
+++ src/net/fexcraft/mod/fvtm/data/part/PartInstallHandler.java
@@ -0,0 +1,34 @@
+package net.fexcraft.mod.fvtm.data.part;
+
+import java.util.HashMap;
+
+import net.fexcraft.app.json.JsonMap;
+import net.fexcraft.mod.fvtm.data.vehicle.VehicleData;
+import net.fexcraft.mod.uni.world.MessageSender;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public abstract class PartInstallHandler {
+
+	public static HashMap<String, PartInstallHandler> HANDLERS = new HashMap<>();
+
+	public abstract boolean validInstall(MessageSender sender, PartData part, String as_category, VehicleData into);
+	
+	public abstract boolean processInstall(MessageSender sender, PartData part, String in_category, VehicleData into);
+
+	public abstract boolean validUninstall(MessageSender sender, PartData part, String is_category, VehicleData from);
+
+	public abstract boolean processUninstall(MessageSender sender, PartData part, String in_category, VehicleData from);
+
+	public abstract boolean allowsCustomCategory(PartData part);
+	
+	public abstract String[] getValidCategories(PartData part, VehicleData vehicle);
+
+	public abstract Object parseData(JsonMap map);
+
+	public static PartInstallHandler getHandler(String id){
+		if(HANDLERS.containsKey(id)) return HANDLERS.get("default");
+		return HANDLERS.get(id);
+	}
+}
