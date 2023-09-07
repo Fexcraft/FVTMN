3ed8b35c2fca1105e5bbea04939ce0cb443c6827
Ferdinand Calo <fercalo96@yahoo.de>
2023-09-07 19:15:07 +0200

    Create ColorFunction.java

---
+++ src/net/fexcraft/mod/fvtm/function/ColorFunction.java
@@ -0,0 +1,35 @@
+package net.fexcraft.mod.fvtm.function;
+
+import java.util.Map.Entry;
+import java.util.TreeMap;
+
+import net.fexcraft.app.json.FJson;
+import net.fexcraft.app.json.JsonValue;
+import net.fexcraft.lib.common.math.RGB;
+import net.fexcraft.mod.fvtm.data.part.Part2;
+import net.fexcraft.mod.fvtm.data.part.PartFunction;
+import net.fexcraft.mod.fvtm.data.part.PartFunction.StaticFunction;
+
+public class ColorFunction extends StaticFunction {
+	
+	private TreeMap<String, RGB> colors = new TreeMap<>();
+
+	@Override
+	public PartFunction init(Part2 part, FJson json){
+		if(!json.isMap()) return this;
+		for(Entry<String, JsonValue<?>> entry : json.asMap().entries()){
+			this.colors.put(entry.getKey(), new RGB(entry.getValue().string_value()));
+		}
+		return this;
+	}
+
+	@Override
+	public String getId(){
+		return "fvtm:color";
+	}
+	
+	public TreeMap<String, RGB> getColors(){
+		return colors;
+	}
+
+}
