372b6dbda88398b66e43c216f646eacf324a0cc7
Ferdinand Calo <fercalo96@yahoo.de>
2023-10-13 21:59:28 +0200

    Update ConstructorMain.java

--- src/net/fexcraft/mod/fvtm/ui/ConstructorMain.java
+++ src/net/fexcraft/mod/fvtm/ui/ConstructorMain.java
@@ -1,6 +1,7 @@
 package net.fexcraft.mod.fvtm.ui;
 
 import net.fexcraft.app.json.JsonMap;
+import net.fexcraft.mod.uni.tag.TagCW;
 import net.fexcraft.mod.uni.ui.ContainerInterface;
 import net.fexcraft.mod.uni.ui.UIButton;
 import net.fexcraft.mod.uni.ui.UserInterface;
@@ -18,7 +19,46 @@ public class ConstructorMain extends UserInterface {
 
 	@Override
 	public boolean onAction(UIButton button, String id, int l, int t, int x, int y, int mb){
-		//
+		switch(id) {
+			case "lift_up": {
+				TagCW com = TagCW.create();
+				com.set("task", "lift");
+				com.set("lift", 1);
+				container.SEND_TO_SERVER.accept(com);
+				break;
+			}
+			case "lift_down": {
+				TagCW com = TagCW.create();
+				com.set("task", "lift");
+				com.set("lift", -1);
+				container.SEND_TO_SERVER.accept(com);
+				break;
+			}
+			case "vehicle_parts": {
+				TagCW com = TagCW.create();
+				com.set("task", "veh_parts");
+				container.SEND_TO_SERVER.accept(com);
+				break;
+			}
+			case "vehicle_attrs": {
+				TagCW com = TagCW.create();
+				com.set("task", "veh_attrs");
+				container.SEND_TO_SERVER.accept(com);
+				break;
+			}
+			case "appearance_texture": {
+				TagCW com = TagCW.create();
+				com.set("task", "appear_tex");
+				container.SEND_TO_SERVER.accept(com);
+				break;
+			}
+			case "appearance_color": {
+				TagCW com = TagCW.create();
+				com.set("task", "appear_color");
+				container.SEND_TO_SERVER.accept(com);
+				break;
+			}
+		}
 		return true;
 	}
 
@@ -30,7 +70,7 @@ public class ConstructorMain extends UserInterface {
 
 	@Override
 	public void predraw(float ticks, int mx, int my){
-		//
+		texts.get("title").translate(container.player.getName());
 	}
 
 	@Override
