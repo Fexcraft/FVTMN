da2d1b3938b8d2dec4d37d473e680fdbff560da8
Ferdinand Calo <fercalo96@yahoo.de>
2023-10-13 21:59:37 +0200

    Create PlayerW.java

---
+++ src/net/fexcraft/mod/uni/world/PlayerW.java
@@ -0,0 +1,14 @@
+package net.fexcraft.mod.uni.world;
+
+import java.util.UUID;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public interface PlayerW {
+
+	public String getName();
+
+	public UUID getUUID();
+
+}
