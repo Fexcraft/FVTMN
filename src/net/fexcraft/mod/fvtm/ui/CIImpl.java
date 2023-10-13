3438f34fc6d17212b832b1152ff5c9d0e3edef58
Ferdinand Calo <fercalo96@yahoo.de>
2023-10-13 21:58:58 +0200

    Update CIImpl.java

--- src/net/fexcraft/mod/fvtm/ui/CIImpl.java
+++ src/net/fexcraft/mod/fvtm/ui/CIImpl.java
@@ -3,14 +3,15 @@ package net.fexcraft.mod.fvtm.ui;
 import net.fexcraft.app.json.JsonMap;
 import net.fexcraft.mod.uni.tag.TagCW;
 import net.fexcraft.mod.uni.ui.ContainerInterface;
+import net.fexcraft.mod.uni.world.PlayerW;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
 public class CIImpl extends ContainerInterface {
 
-	public CIImpl(JsonMap map){
-		super(map);
+	public CIImpl(JsonMap map, PlayerW player){
+		super(map, player);
 	}
 
 	@Override
