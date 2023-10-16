2117be97e00d29687e142d45b7c1722051d1c015
Ferdinand Calo <fercalo96@yahoo.de>
2023-10-16 21:03:31 +0200

    Update ParticleEmitterFunction.java

--- src/net/fexcraft/mod/fvtm/function/ParticleEmitterFunction.java
+++ src/net/fexcraft/mod/fvtm/function/ParticleEmitterFunction.java
@@ -7,7 +7,7 @@ import net.fexcraft.app.json.FJson;
 import net.fexcraft.app.json.JsonMap;
 import net.fexcraft.app.json.JsonValue;
 import net.fexcraft.lib.common.math.V3D;
-import net.fexcraft.lib.mc.utils.Formatter;
+import net.fexcraft.lib.common.utils.Formatter;
 import net.fexcraft.mod.fvtm.FvtmRegistry;
 import net.fexcraft.mod.fvtm.data.part.Part;
 import net.fexcraft.mod.fvtm.data.part.PartData;
