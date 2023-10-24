1dfb6ebd10b3eed867ee302c719783b345030fbe
Ferdinand Calo <fercalo96@yahoo.de>
2023-10-25 00:59:07 +0200

    Create WheelTireData.java

---
+++ src/net/fexcraft/mod/fvtm/sys/uni/WheelTireData.java
@@ -0,0 +1,20 @@
+package net.fexcraft.mod.fvtm.sys.uni;
+
+import net.fexcraft.lib.common.math.V3D;
+import net.fexcraft.mod.fvtm.util.function.TireFunction.TireAttr;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class WheelTireData {
+
+	public final String id;
+	public V3D pos;
+	public Axle axle;
+	public TireAttr function;
+
+	public WheelTireData(String key){
+		this.id = key;
+	}
+
+}
