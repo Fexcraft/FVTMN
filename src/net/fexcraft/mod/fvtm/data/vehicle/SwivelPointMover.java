030f4cdd8bb2ab056b05659cd9625701b264d166
Ferdinand Calo <fercalo96@yahoo.de>
2023-10-29 12:03:28 +0100

    Update SwivelPointMover.java

--- src/net/fexcraft/mod/fvtm/data/vehicle/SwivelPointMover.java
+++ src/net/fexcraft/mod/fvtm/data/vehicle/SwivelPointMover.java
@@ -1,8 +1,10 @@
 package net.fexcraft.mod.fvtm.data.vehicle;
 
+import net.fexcraft.mod.fvtm.sys.uni.VehicleInstance;
+
 public interface SwivelPointMover {
 	
-	public void update(VehicleEntity entity, SwivelPoint point);
+	public void update(VehicleInstance vehicle, SwivelPoint point);
 	
 	public SwivelPointMover clone();
 	
