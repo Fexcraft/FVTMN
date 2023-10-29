ffcb62fbf202b8eb19d6b4fd1d01d37f342d611c
Ferdinand Calo <fercalo96@yahoo.de>
2023-10-29 16:51:19 +0100

    Update PartSlots.java

--- src/net/fexcraft/mod/fvtm/data/part/PartSlots.java
+++ src/net/fexcraft/mod/fvtm/data/part/PartSlots.java
@@ -1,33 +1,29 @@
 package net.fexcraft.mod.fvtm.data.part;
 
-import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map.Entry;
 
-import net.fexcraft.app.json.JsonArray;
 import net.fexcraft.app.json.JsonMap;
+import net.fexcraft.app.json.JsonValue;
 import net.fexcraft.mod.uni.EnvInfo;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
-public class PartSlots extends ArrayList<PartSlot> {
+public class PartSlots extends LinkedHashMap<String, PartSlot> {
 
 	public HashMap<String, Integer> count;
 	public boolean copy_rot;
 
-	public PartSlots(String cat, JsonMap obj){
-		this(cat, obj.get("slots").asArray());
-		copy_rot = obj.has("copy_rot") ? obj.get("copy_rot").bool() : false;
-	}
-
-	public PartSlots(String defcat, JsonArray jslots){
-		for(int i = 0; i < jslots.size(); i++){
-			JsonArray array = jslots.get(i).asArray();
-			add(new PartSlot(defcat, array, i));
+	public PartSlots(JsonMap map){
+		copy_rot = map.has("copy_rot") ? map.rem("copy_rot").bool() : false;
+		for(Entry<String, JsonValue<?>> entry : map.entries()){
+			put(entry.getKey(), new PartSlot(entry.getKey(), entry.getValue()));
 		}
 		if(EnvInfo.CLIENT){
 			count = new HashMap<>();
-			for(PartSlot slot : this){
+			for(PartSlot slot : this.values()){
 				if(count.containsKey(slot.type)) count.put(slot.type, count.get(slot.type) + 1);
 				else count.put(slot.type, 1);
 			}
