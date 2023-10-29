3b5fded4aa1dc8787ed125a48db93144cd237420
Ferdinand Calo <fercalo96@yahoo.de>
2023-10-29 13:48:59 +0100

    Update MessageSender.java

--- src/net/fexcraft/mod/uni/world/MessageSender.java
+++ src/net/fexcraft/mod/uni/world/MessageSender.java
@@ -1,5 +1,7 @@
 package net.fexcraft.mod.uni.world;
 
+import net.fexcraft.mod.fvtm.FvtmLogger;
+
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
@@ -10,4 +12,24 @@ public interface MessageSender {
 	public void bar(String s);
 
 	public void dismount();
+
+	public static final MessageSender LOG = new MessageSender(){
+
+		@Override
+		public void send(String s){
+			FvtmLogger.LOGGER.log(s);
+		}
+
+		@Override
+		public void bar(String s){
+			FvtmLogger.LOGGER.log(s);
+		}
+
+		@Override
+		public void dismount(){
+			//
+		}
+
+	};
+
 }
