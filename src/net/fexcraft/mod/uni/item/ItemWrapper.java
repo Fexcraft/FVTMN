facbf8d15f71eca5d098a82646062ea5a4a1b084
Ferdinand Calo <fexcalo@gmail.com>
2023-11-12 21:24:16 +0100

    Update ItemWrapper.java

--- src/net/fexcraft/mod/uni/item/ItemWrapper.java
+++ src/net/fexcraft/mod/uni/item/ItemWrapper.java
@@ -4,6 +4,8 @@ public abstract class ItemWrapper {
 
 	public abstract void linkContainer();
 
+	public abstract void regToDict();
+
 	public abstract <LI> LI local();
 
 	public abstract Object direct();
