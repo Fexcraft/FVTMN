56499d0d5178ee378507027915f039234090ab3a
Ferdinand Calo <fexcalo@gmail.com>
2023-11-24 19:38:30 +0100

    Update Packets.java

--- src/net/fexcraft/mod/fvtm/packet/Packets.java
+++ src/net/fexcraft/mod/fvtm/packet/Packets.java
@@ -1,6 +1,9 @@
 package net.fexcraft.mod.fvtm.packet;
 
+import net.fexcraft.lib.common.math.V3I;
+import net.fexcraft.mod.fvtm.data.block.BlockData;
 import net.fexcraft.mod.fvtm.sys.uni.SeatInstance;
+import net.fexcraft.mod.uni.world.WorldW;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
@@ -13,5 +16,11 @@ public abstract class Packets {
 
     /** Send Seat Update Packet to Server */
     public abstract void send(SeatInstance seat);
+
+    /** Send BlockData Update Packet to all around. */
+    public abstract void send(BlockData blockdata, Object pos, int dim);
+
+    /** Send BlockData Update Packet to all around. */
+    public abstract void send(WorldW world, V3I pos);
 
 }
