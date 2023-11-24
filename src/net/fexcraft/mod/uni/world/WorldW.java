08dc9c69305e3c21d82fa4e1c132c8fa87d3cbfa
Ferdinand Calo <fexcalo@gmail.com>
2023-11-24 19:38:46 +0100

    Update WorldW.java

--- src/net/fexcraft/mod/uni/world/WorldW.java
+++ src/net/fexcraft/mod/uni/world/WorldW.java
@@ -1,10 +1,27 @@
 package net.fexcraft.mod.uni.world;
 
+import net.fexcraft.lib.common.math.V3D;
+import net.fexcraft.lib.common.math.V3I;
+import net.fexcraft.mod.fvtm.data.block.BlockEntity;
+import net.minecraft.world.World;
+
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
 public abstract class WorldW {
 
 	public abstract boolean isClient();
+
+    public abstract boolean isTilePresent(V3I pos);
+
+    public abstract BlockEntity getBlockEntity(V3I pos);
+
+    public abstract <W> W local();
+
+    public abstract Object direct();
+
+    public abstract void setBlockState(V3I pos, StateWrapper state, int flag);
+
+    public abstract void spawnBlockSeat(V3D add, EntityW player);
 
 }
