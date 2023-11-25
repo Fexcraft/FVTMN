6fd76180cb4758a426fc1ce8072776aa4c8a5ac0
Ferdinand Calo <fexcalo@gmail.com>
2023-11-25 21:25:26 +0100

    Update AABB.java

--- src/net/fexcraft/mod/fvtm/data/block/AABB.java
+++ src/net/fexcraft/mod/fvtm/data/block/AABB.java
@@ -1,20 +1,33 @@
 package net.fexcraft.mod.fvtm.data.block;
 
+import net.minecraft.util.math.AxisAlignedBB;
+
 import java.util.ArrayList;
+import java.util.function.Supplier;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
-public class AABB {
+public abstract class AABB {
 
-    public static final AABB[] NULL = new AABB[]{ null };
-    public static final AABB[] FULL = new AABB[]{ new AABB(0, 0, 0, 1, 1, 1) };
-    public float x0, y0, z0;
-    public float x1, y1, z1;
+    public static Supplier<AABB> SUPPLIER = null;
+    //public float x0, y0, z0;
+    //public float x1, y1, z1;
 
-    public AABB(float sx, float sy, float sz, float ex, float ey, float ez){
-        x0 = sx; y0 = sy; z0 = sz;
-        x1 = ex; y1 = ey; z1 = ez;
+    public static AABB create(){
+        return SUPPLIER.get();
     }
+
+    public static AABB create(float sx, float sy, float sz, float ex, float ey, float ez){
+        return create().set(sx, sy, sz, ex, sy, sz);
+    }
+
+    public abstract AABB set(float sx, float sy, float sz, float ex, float ey, float ez);
+
+    public abstract <AB> AB local();
+
+    public abstract Object direct();
+
+    public abstract <AB> AB offset(int x, int y, int z);
 
 }
