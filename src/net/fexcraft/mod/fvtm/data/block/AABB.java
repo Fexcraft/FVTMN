a2196a2b2b3e8e700d97dd66d4e8dc2960b3bd21
Ferdinand Calo <fexcalo@gmail.com>
2023-11-27 23:52:22 +0100

    Update AABB.java

--- src/net/fexcraft/mod/fvtm/data/block/AABB.java
+++ src/net/fexcraft/mod/fvtm/data/block/AABB.java
@@ -15,11 +15,11 @@ public abstract class AABB {
     //public float x1, y1, z1;
 
     public static AABB create(){
-        return SUPPLIER.get();
+        return SUPPLIER.get().set(0, 0, 0, 1, 1, 1);
     }
 
     public static AABB create(float sx, float sy, float sz, float ex, float ey, float ez){
-        return create().set(sx, sy, sz, ex, sy, sz);
+        return SUPPLIER.get().set(sx, sy, sz, ex, ey, ez);
     }
 
     public abstract AABB set(float sx, float sy, float sz, float ex, float ey, float ez);
@@ -29,5 +29,10 @@ public abstract class AABB {
     public abstract Object direct();
 
     public abstract <AB> AB offset(int x, int y, int z);
+
+    @Override
+    public String toString(){
+        return direct().toString();
+    }
 
 }
