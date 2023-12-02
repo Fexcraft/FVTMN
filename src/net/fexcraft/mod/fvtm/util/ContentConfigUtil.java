0059af33f0c2de395aa74b228dfbe16e5f6693d8
Ferdinand Calo <fexcalo@gmail.com>
2023-12-02 19:29:05 +0100

    Update ContentConfigUtil.java

--- src/net/fexcraft/mod/fvtm/util/ContentConfigUtil.java
+++ src/net/fexcraft/mod/fvtm/util/ContentConfigUtil.java
@@ -37,54 +37,6 @@ public class ContentConfigUtil {
 		else return IDLManager.getIDLCached((pack == null ? "fvtm" : pack.getID().id()) + ":" + id);
 	}
 
-	public static ModelData getModelData(JsonMap map, String key, ModelData data){
-		if(key == null) key = "ModelData";
-		if(map.has(key)){
-			for(Entry<String, JsonValue<?>> entry : map.getMap(key).entries()){
-				getModelDataEntry(data, entry.getKey(), entry.getValue());
-			}
-		}
-		return data;
-	}
-
-	private static void getModelDataEntry(ModelData data, String key, JsonValue value){
-		if(value.isArray()){
-			ArrayList<Object> list = data.containsKey(key) ? data.get(key) : new ArrayList<>();
-			for(JsonValue<?> val : value.asArray().value){
-				if(val.isValue()){
-					list.add(val.string_value());
-				}
-				else if(key.equals(Model.PROGRAMS)){
-					if(val.isArray()){
-						JsonArray array = val.asArray();
-						String group = array.get(0).string_value();
-						array.value.remove(0);
-						list.add(new Object[]{ group, array });
-					}
-					else{
-						JsonMap prog = val.asMap();
-						String group = prog.get("group").string_value();
-						prog.rem("group");
-						list.add(new Object[]{ group, prog });
-					}
-				}
-				else {
-					list.add(val);
-				}
-			}
-			data.set(key, list);
-		}
-		else if(value.isValue()){
-			if(value.isBoolean()){
-				data.set(key, value.bool());
-			}
-			else if(value.isNumber()){
-				data.set(key, value.float_value());
-			}
-			else data.set(key, value.string_value());
-		}
-	}
-
 	public static Addon getAddon(JsonMap map){
 		if(map.has("Addon")){
 			String id = map.get("Addon").string_value();
