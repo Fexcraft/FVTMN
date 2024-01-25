84cf8cd89792186e62ba7330000d09a1f4bfa901
Ferdinand Calo <fexcalo@gmail.com>
2024-01-25 13:23:39 +0100

    Update RenderCache.java

--- src/net/fexcraft/mod/fvtm/model/RenderCache.java
+++ src/net/fexcraft/mod/fvtm/model/RenderCache.java
@@ -2,6 +2,7 @@ package net.fexcraft.mod.fvtm.model;
 
 import java.util.Map;
 import java.util.Set;
+import java.util.function.Supplier;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
@@ -10,17 +11,26 @@ import java.util.Set;
  */
 public interface RenderCache {
 
-	public Map<String, Float> map();
+	public Map<String, Float> fltmap();
 
-	public Set<String> keys();
+	public Map<String, Object> map();
 	
 	/** Gets a float value if present, else returns null. */
-	public Float get(String id);
+	public Float getFlt(String id);
 	
 	/** Returns the specified default value if entry is missing. */
-	public Float get(String id, Float def);
+	public Float getFlt(String id, Float def);
 	
 	/** Set value to `null` to remove the entry. Otherwise, it updates the cache. */
-	public Float set(String id, Float value);
+	public Float setFlt(String id, Float value);
+
+	/** Gets a object if present, else returns null. */
+	public <V> V get(String id);
+
+	/** Returns a new default object if entry is missing. */
+	public <V> V get(String id, Supplier<V> def);
+
+	/** Set object to `null` to remove the entry. Otherwise, it updates the cache. */
+	public <V> V set(String id, V value);
 
 }
