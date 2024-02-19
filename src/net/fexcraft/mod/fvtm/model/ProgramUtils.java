19339c2f8febb310b16b4ea3688f28f51786cff2
Ferdinand Calo <fexcalo@gmail.com>
2024-02-19 21:47:58 +0100

    Update ProgramUtils.java

--- src/net/fexcraft/mod/fvtm/model/ProgramUtils.java
+++ src/net/fexcraft/mod/fvtm/model/ProgramUtils.java
@@ -11,6 +11,7 @@ public class ProgramUtils {
 	public static final Function<ModelRenderData, Float[]> FLOAT2_SUPP = mrd -> new Float[]{ 0f, 0f };
 	public static final Function<ModelRenderData, Integer> INT_SUPP = mrd -> 0;
 	public static final Function<ModelRenderData, FloatBool> FLOAT_BOOL_SUPP = mrd -> new FloatBool(0, false);
+	public static final Function<ModelRenderData, FloatBool2> FLOAT_BOOL2_SUPP = mrd -> new FloatBool2(0, false, false);
 
 	public static class FloatBool {
 
@@ -20,6 +21,20 @@ public class ProgramUtils {
 		public FloatBool(float f, boolean b){
 			fl = f;
 			bl = b;
+		}
+
+	}
+
+	public static class FloatBool2 {
+
+		public float fl;
+		public boolean bl0;
+		public boolean bl1;
+
+		public FloatBool2(float f, boolean b0, boolean b1){
+			fl = f;
+			bl0 = b0;
+			bl1 = b1;
 		}
 
 	}
