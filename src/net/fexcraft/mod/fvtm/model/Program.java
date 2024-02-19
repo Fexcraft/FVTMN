eb304b56aa79f0109125f100ae5072fa12570920
Ferdinand Calo <fexcalo@gmail.com>
2024-02-19 21:47:54 +0100

    Update Program.java

--- src/net/fexcraft/mod/fvtm/model/Program.java
+++ src/net/fexcraft/mod/fvtm/model/Program.java
@@ -33,7 +33,13 @@ public interface Program {
 
 	public default RenderOrder order(){ return RenderOrder.NORMAL; }
 
-	public default Program mirror(){ return this; }
+	//public default Program mirror(){ return this; }
+
+	public default int ticktime(){ return 0; }
+
+	public default void reverse(ModelRenderData data){}
+
+	public default Program pause(ModelRenderData data, boolean pause){ return this; }
 
 	public default void reset(ModelRenderData data){
 		data.cache.set(this, null);
