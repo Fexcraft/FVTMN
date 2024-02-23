138256be1499a78e9c2e9a7f146f12f5c7eb5bb0
Ferdinand Calo <fexcalo@gmail.com>
2024-02-23 19:24:38 +0100

    Update FvtmResources.java

--- src/net/fexcraft/mod/fvtm/FvtmResources.java
+++ src/net/fexcraft/mod/fvtm/FvtmResources.java
@@ -420,7 +420,7 @@ public abstract class FvtmResources {
 
 	public static Model getEmptyModelForClass(Class<? extends Model> clazz){
 		if(clazz == ContainerModel.class) return ContainerModel.EMPTY;
-		if(clazz == PartModel.class) return PartModel.EMPTY;
+		//if(clazz == PartModel.class) return PartModel.EMPTY;
 		if(clazz == VehicleModel.class) return VehicleModel.EMPTY;
 		if(clazz == TrafficSignModel.class) return TrafficSignModel.EMPTY;
 		if(clazz == BlockModel.class) return BlockModel.EMPTY;
