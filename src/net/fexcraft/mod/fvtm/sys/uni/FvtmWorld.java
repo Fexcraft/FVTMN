81c8bf40371632e6d10603964ae142e005dc3ee5
Ferdinand Calo <fexcalo@gmail.com>
2024-02-24 23:09:22 +0100

    Update FvtmWorld.java

--- src/net/fexcraft/mod/fvtm/sys/uni/FvtmWorld.java
+++ src/net/fexcraft/mod/fvtm/sys/uni/FvtmWorld.java
@@ -1,9 +1,11 @@
 package net.fexcraft.mod.fvtm.sys.uni;
 
+import net.fexcraft.mod.fvtm.data.vehicle.SwivelPoint;
 import net.fexcraft.mod.uni.world.WorldW;
 
 public abstract class FvtmWorld extends WorldW {
 
 	public abstract SeatInstance getSeat(int entid, int seatid);
 
+	public abstract SwivelPoint getSwivelPoint(int entid, String pointid);
 }
