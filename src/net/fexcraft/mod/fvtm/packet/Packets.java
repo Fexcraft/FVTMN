344976ff7de9c5a647ae22e73bd4363c54dafd64
Ferdinand Calo <fexcalo@gmail.com>
2024-02-25 09:53:16 +0100

    Update Packets.java

--- src/net/fexcraft/mod/fvtm/packet/Packets.java
+++ src/net/fexcraft/mod/fvtm/packet/Packets.java
@@ -1,5 +1,6 @@
 package net.fexcraft.mod.fvtm.packet;
 
+import io.netty.buffer.ByteBuf;
 import net.fexcraft.lib.common.math.V3D;
 import net.fexcraft.lib.common.math.V3I;
 import net.fexcraft.mod.fvtm.data.block.BlockData;
@@ -8,9 +9,12 @@ import net.fexcraft.mod.fvtm.sys.uni.SeatInstance;
 import net.fexcraft.mod.fvtm.sys.uni.VehicleInstance;
 import net.fexcraft.mod.uni.tag.TagCW;
 import net.fexcraft.mod.uni.world.WorldW;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.nbt.NBTTagCompound;
 
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.function.BiConsumer;
 
 import static net.fexcraft.mod.fvtm.Config.VEHICLE_UPDATE_RANGE;
 
@@ -19,45 +23,60 @@ import static net.fexcraft.mod.fvtm.Config.VEHICLE_UPDATE_RANGE;
  */
 public abstract class Packets {
 
-    public static Packets INSTANCE = null;
+	public static HashMap<String, PacketListener> LIS_CLIENT = new HashMap<>();
+	public static HashMap<String, PacketListener> LIS_SERVER = new HashMap<>();
+	public static Packets INSTANCE = null;
 
-    public abstract void init();
+	public abstract void init();
 
-    /** Send BlockData Update Packet to all around. */
-    public abstract void send(BlockData blockdata, V3I pos, int dim);
+	public abstract void writeTag(ByteBuf buffer, TagCW tag);
 
-    /** Send BlockData Update Packet to all around. */
-    public abstract void send(WorldW world, V3I pos);
+	public abstract TagCW readTag(ByteBuf buffer);
 
-    /** Send generic nbt packet to a vehicle. */
-    public abstract void send(VehicleInstance vehicleInstance, TagCW com);
+	/** Send BlockData Update Packet to all around. */
+	public abstract void send(BlockData blockdata, V3I pos, int dim);
 
-    /** Sends a Packet to the Server. */
-    public abstract void send0(Class<? extends PacketBase> packet, Object... data);
+	/** Send BlockData Update Packet to all around. */
+	public abstract void send(WorldW world, V3I pos);
 
-    /** Sends a Packet to the Server. */
-    public static void send(Class<? extends PacketBase> packet, Object... data){
-        INSTANCE.send0(packet, data);
-    }
+	/** Send generic nbt packet to a vehicle. */
+	public abstract void send(VehicleInstance vehicleInstance, TagCW com);
 
-    /** Sends a Packet to all in range. */
-    public abstract void sendInRange0(Class<? extends PacketBase> packet, WorldW world, V3D pos, int range, Object... data);
+	/** Sends a Packet to the Server. */
+	public abstract void send0(Class<? extends PacketBase> packet, Object... data);
 
-    /** Sends a Packet to all in range. */
-    public static void sendInRange(Class<? extends PacketBase> packet, WorldW world, V3D pos, int range, Object... data){
-        INSTANCE.sendInRange0(packet, world, pos, range, data);
-    }
-    /** Sends a Packet to all in range. */
-    public static void sendInRange(Class<? extends PacketBase> packet, Passenger pass, Object... data){
-        sendInRange(packet, pass.getWorld(), pass.getPos(), VEHICLE_UPDATE_RANGE, data);
-    }
+	/** Sends a Packet to the Server. */
+	public static void send(Class<? extends PacketBase> packet, Object... data){
+		INSTANCE.send0(packet, data);
+	}
 
-    /** Sends a Packet to all. */
-    public abstract void sendToAll0(Class<? extends PacketBase> packet, Object... data);
+	/** Sends a Packet to all in range. */
+	public abstract void sendInRange0(Class<? extends PacketBase> packet, WorldW world, V3D pos, int range, Object... data);
 
-    /** Sends a Packet to all. */
-    public static void sendToAll(Class<? extends PacketBase> packet, Object... data){
-        INSTANCE.sendToAll0(packet, data);
-    }
+	/** Sends a Packet to all in range. */
+	public static void sendInRange(Class<? extends PacketBase> packet, WorldW world, V3D pos, int range, Object... data){
+		INSTANCE.sendInRange0(packet, world, pos, range, data);
+	}
+
+	/** Sends a Packet to all in range. */
+	public static void sendInRange(Class<? extends PacketBase> packet, Passenger pass, Object... data){
+		sendInRange(packet, pass.getWorld(), pass.getPos(), VEHICLE_UPDATE_RANGE, data);
+	}
+
+	/** Sends a Packet to all. */
+	public abstract void sendToAll0(Class<? extends PacketBase> packet, Object... data);
+
+	/** Sends a Packet to all. */
+	public static void sendToAll(Class<? extends PacketBase> packet, Object... data){
+		INSTANCE.sendToAll0(packet, data);
+	}
+
+	/** Sends a Packet to the Server. */
+	public abstract void sendTo0(Class<? extends PacketBase> packet, Passenger to, Object... data);
+
+	/** Sends a Packet to the Server. */
+	public static void sendTo(Class<? extends PacketBase> packet, Passenger to, Object... data){
+		INSTANCE.sendTo0(packet, to, data);
+	}
 
 }
