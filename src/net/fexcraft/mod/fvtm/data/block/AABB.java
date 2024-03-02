16d52cd1f67ce3284fbebeab38fb76937b202203
Ferdinand Calo <fexcalo@gmail.com>
2024-03-02 13:28:55 +0100

    Update AABB.java

--- src/net/fexcraft/mod/fvtm/data/block/AABB.java
+++ src/net/fexcraft/mod/fvtm/data/block/AABB.java
@@ -1,5 +1,7 @@
 package net.fexcraft.mod.fvtm.data.block;
 
+import net.fexcraft.lib.common.math.V3D;
+
 import java.util.function.Supplier;
 
 /**
@@ -19,7 +21,23 @@ public abstract class AABB {
         return SUPPLIER.get().set(sx, sy, sz, ex, ey, ez);
     }
 
+    public static AABB create(double sx, double sy, double sz, double ex, double ey, double ez){
+        return SUPPLIER.get().set(sx, sy, sz, ex, ey, ez);
+    }
+
+    public static AABB create(float[] arr){
+        return SUPPLIER.get().set(arr);
+    }
+
     public abstract AABB set(float sx, float sy, float sz, float ex, float ey, float ez);
+
+    public AABB set(double sx, double sy, double sz, double ex, double ey, double ez){
+        return set((float)sx, (float)sy, (float)sz, (float)ex, (float)ey, (float)ez);
+    }
+
+    public AABB set(float[] arr){
+        return set(arr[0], arr[1], arr[2], arr[3], arr[4], arr[5]);
+    }
 
     public abstract <AB> AB local();
 
@@ -31,5 +49,9 @@ public abstract class AABB {
     public String toString(){
         return direct().toString();
     }
+
+    public abstract boolean contains(V3D vec);
+
+    public abstract boolean contains(Object vec);
 
 }
