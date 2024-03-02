af32867448a697502bea5bdb533e1e966858fca8
Ferdinand Calo <fexcalo@gmail.com>
2024-03-02 17:18:21 +0100

    Update UIKey.java

--- src/net/fexcraft/mod/fvtm/ui/UIKey.java
+++ src/net/fexcraft/mod/fvtm/ui/UIKey.java
@@ -10,11 +10,17 @@ public class UIKey {
 	public static final int ID12_BLOCK_INVENTORY_ITEM = 961;
 	public static final int ID12_BLOCK_INVENTORY_FLUID = 962;
 	public static final int ID12_BLOCK_INVENTORY_VAR = 963;
+	public static final int ID12_MULTIBLOCK_INVENTORY_ITEM = 9511;
+	public static final int ID12_MULTIBLOCK_INVENTORY_FLUID = 9512;
+	public static final int ID12_MULTIBLOCK_INVENTORY_VAR = 9513;
 	public static final UIKey VEHICLE_MAIN = new UIKey(ID12_VEHICLE_MAIN, "fvtm:vehicle_main");
 	public static final UIKey DECORATION_EDITOR = new UIKey(ID12_DECORATION_EDITOR, "fvtm:decoration_editor");
 	public static final UIKey BLOCK_INVENTORY_ITEM = new UIKey(ID12_BLOCK_INVENTORY_ITEM, "fvtm:block_inventory_item");
 	public static final UIKey BLOCK_INVENTORY_FLUID = new UIKey(ID12_BLOCK_INVENTORY_FLUID, "fvtm:block_inventory_fluid");
 	public static final UIKey BLOCK_INVENTORY_VAR = new UIKey(ID12_BLOCK_INVENTORY_VAR, "fvtm:block_inventory_var");
+	public static final UIKey MULTIBLOCK_INVENTORY_ITEM = new UIKey(ID12_MULTIBLOCK_INVENTORY_ITEM, "fvtm:mb_inventory_item");
+	public static final UIKey MULTIBLOCK_INVENTORY_FLUID = new UIKey(ID12_MULTIBLOCK_INVENTORY_FLUID, "fvtm:mb_inventory_fluid");
+	public static final UIKey MULTIBLOCK_INVENTORY_VAR = new UIKey(ID12_MULTIBLOCK_INVENTORY_VAR, "fvtm:mb_inventory_var");
 
 	public final int id;
 	public final String key;
