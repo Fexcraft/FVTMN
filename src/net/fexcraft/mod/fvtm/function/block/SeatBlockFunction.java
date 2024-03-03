dfc726622405390dd9648b80e13647b7844785ed
Ferdinand Calo <fexcalo@gmail.com>
2024-03-03 12:52:32 +0100

    Update SeatBlockFunction.java

--- src/net/fexcraft/mod/fvtm/function/block/SeatBlockFunction.java
+++ src/net/fexcraft/mod/fvtm/function/block/SeatBlockFunction.java
@@ -8,6 +8,7 @@ import net.fexcraft.lib.common.math.V3I;
 import net.fexcraft.lib.common.utils.Formatter;
 import net.fexcraft.mod.fvtm.data.block.BlockData;
 import net.fexcraft.mod.fvtm.data.block.BlockFunction;
+import net.fexcraft.mod.fvtm.sys.uni.Passenger;
 import net.fexcraft.mod.fvtm.util.ContentConfigUtil;
 import net.fexcraft.mod.uni.item.StackWrapper;
 import net.fexcraft.mod.uni.world.CubeSide;
@@ -43,7 +44,7 @@ public class SeatBlockFunction extends BlockFunction.StaticBlockFunction {
     }
 
     @Override
-    public boolean onClick(WorldW world, V3I pos, V3D hit, StateWrapper state, CubeSide side, EntityW player, boolean main){
+    public boolean onClick(WorldW world, V3I pos, V3D hit, StateWrapper state, CubeSide side, Passenger player, boolean main){
         if(!main) return false;
         if(!player.isRiding()){
             world.spawnBlockSeat(offset.add(pos.x + 0.5, pos.y, pos.z + 0.5), player);
