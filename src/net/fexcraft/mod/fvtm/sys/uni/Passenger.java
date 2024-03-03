dd6bc4b07c3f4908325908c989d4f3c2ca54514d
Ferdinand Calo <fexcalo@gmail.com>
2024-03-03 12:52:49 +0100

    Update Passenger.java

--- src/net/fexcraft/mod/fvtm/sys/uni/Passenger.java
+++ src/net/fexcraft/mod/fvtm/sys/uni/Passenger.java
@@ -1,5 +1,7 @@
 package net.fexcraft.mod.fvtm.sys.uni;
 
+import net.fexcraft.lib.common.math.V3I;
+import net.fexcraft.mod.fvtm.ui.UIKey;
 import net.fexcraft.mod.uni.world.EntityW;
 
 /**
@@ -12,6 +14,8 @@ public abstract class Passenger extends EntityW {
 	public FvtmWorld getFvtmWorld(){
 		return (FvtmWorld)getWorld();
 	}
+
+	public abstract void openUI(UIKey key, V3I pos);
 
 	public abstract void set(int veh, int seatid);
 
