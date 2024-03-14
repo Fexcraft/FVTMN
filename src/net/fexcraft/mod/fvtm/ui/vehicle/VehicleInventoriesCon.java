4b7214889064c67679f75f8a3fefee20878c7038
Ferdinand Calo <fexcalo@gmail.com>
2024-03-14 20:58:41 +0100

    Create VehicleInventoriesCon.java

---
+++ src/net/fexcraft/mod/fvtm/ui/vehicle/VehicleInventoriesCon.java
@@ -0,0 +1,39 @@
+package net.fexcraft.mod.fvtm.ui.vehicle;
+
+import net.fexcraft.app.json.JsonMap;
+import net.fexcraft.lib.common.math.V3I;
+import net.fexcraft.mod.fvtm.sys.uni.Passenger;
+import net.fexcraft.mod.fvtm.sys.uni.VehicleInstance;
+import net.fexcraft.mod.fvtm.ui.UIKey;
+import net.fexcraft.mod.fvtm.util.function.InventoryFunction;
+import net.fexcraft.mod.uni.tag.TagCW;
+import net.fexcraft.mod.uni.ui.ContainerInterface;
+import net.fexcraft.mod.uni.world.EntityW;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class VehicleInventoriesCon extends ContainerInterface {
+
+	protected VehicleInstance vehicle;
+
+	public VehicleInventoriesCon(JsonMap map, EntityW player, V3I pos){
+		super(map, player, pos);
+		vehicle = ((Passenger)player).getFvtmWorld().getVehicle(pos.x);
+	}
+
+	@Override
+	public Object get(String key, Object... objs){
+		if(key.equals("vehicle")) return vehicle;
+		return null;
+	}
+
+	@Override
+	public void packet(TagCW com, boolean client){
+		if(client || !com.has("open")) return;
+		InventoryFunction func = vehicle.data.getFunctionInPart(com.getString("open"), "fvtm:inventory");
+		UIKey ui = func.inventory().type.isItem() ? UIKey.VEHICLE_INVENTORY_ITEM : UIKey.VEHICLE_INVENTORY_FLUID;
+		((Passenger)player).openUI(ui, pos.add(0, vehicle.data.getInventories().indexOf(com.getString("open")), 0));
+	}
+
+}
