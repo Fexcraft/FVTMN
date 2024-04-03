86cb9c112ef120f2ac71c058dc9a97d70b74b423
Ferdinand Calo <fexcalo@gmail.com>
2024-04-03 21:49:08 +0200

    Create VehicleCatalogCon.java

---
+++ src/net/fexcraft/mod/fvtm/ui/vehicle/VehicleCatalogCon.java
@@ -0,0 +1,17 @@
+package net.fexcraft.mod.fvtm.ui.vehicle;
+
+import net.fexcraft.app.json.JsonMap;
+import net.fexcraft.lib.common.math.V3I;
+import net.fexcraft.mod.uni.ui.ContainerInterface;
+import net.fexcraft.mod.uni.world.EntityW;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class VehicleCatalogCon extends ContainerInterface {
+
+	public VehicleCatalogCon(JsonMap map, EntityW ply, V3I pos){
+		super(map, ply, pos);
+	}
+
+}
