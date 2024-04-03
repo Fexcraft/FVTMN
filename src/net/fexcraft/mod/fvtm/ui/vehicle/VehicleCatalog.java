4db86d5cc769114f3bb1b7413d751097d57951ae
Ferdinand Calo <fexcalo@gmail.com>
2024-04-03 22:58:25 +0200

    Update VehicleCatalog.java

--- src/net/fexcraft/mod/fvtm/ui/vehicle/VehicleCatalog.java
+++ src/net/fexcraft/mod/fvtm/ui/vehicle/VehicleCatalog.java
@@ -1,30 +1,47 @@
 package net.fexcraft.mod.fvtm.ui.vehicle;
 
 import net.fexcraft.app.json.JsonMap;
+import net.fexcraft.mod.fvtm.FvtmLogger;
 import net.fexcraft.mod.fvtm.FvtmRegistry;
 import net.fexcraft.mod.fvtm.data.addon.Addon;
 import net.fexcraft.mod.fvtm.data.attribute.Attribute;
+import net.fexcraft.mod.fvtm.data.part.PartSlots;
 import net.fexcraft.mod.fvtm.data.vehicle.Vehicle;
+import net.fexcraft.mod.fvtm.data.vehicle.VehicleData;
 import net.fexcraft.mod.uni.tag.TagCW;
 import net.fexcraft.mod.uni.ui.ContainerInterface;
 import net.fexcraft.mod.uni.ui.UIButton;
 import net.fexcraft.mod.uni.ui.UserInterface;
+import net.minecraft.client.resources.I18n;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
 public class VehicleCatalog extends UserInterface {
 
-	private ArrayList<Addon> vehpacks = new ArrayList<Addon>();
+	private ArrayList<Addon> vehpacks = new ArrayList<>();
+	private HashMap<Addon, ArrayList<Vehicle>> vehicles = new LinkedHashMap<>();
+	private VehicleData data;
+	private Vehicle veh;
 	private int pack;
+	private int vehicle;
+	private int recipe;
 
 	public VehicleCatalog(JsonMap map, ContainerInterface container) throws Exception{
 		super(map, container);
 		for(Vehicle veh : FvtmRegistry.VEHICLES){
-			if(!vehpacks.contains(veh.getAddon())) vehpacks.add(veh.getAddon());
+			if(!vehpacks.contains(veh.getAddon())){
+				vehpacks.add(veh.getAddon());
+				vehicles.put(veh.getAddon(), new ArrayList<>());
+			}
+			vehicles.get(veh.getAddon()).add(veh);
 		}
+		switchPack(0);
 	}
 
 	@Override
@@ -38,6 +55,14 @@ public class VehicleCatalog extends UserInterface {
 				switchPack(1);
 				return true;
 			}
+			case "veh_prev":{
+				switchVehicle(-1);
+				return true;
+			}
+			case "veh_next":{
+				switchVehicle(1);
+				return true;
+			}
 		}
 		return false;
 	}
@@ -47,6 +72,30 @@ public class VehicleCatalog extends UserInterface {
 		if(pack >= vehpacks.size()) pack = 0;
 		if(pack < 0) pack = vehpacks.size() - 1;
 		texts.get("pack").value(vehpacks.get(pack).getName());
+		switchVehicle(0);
+	}
+
+	private void switchVehicle(int i){
+		vehicle += i;
+		ArrayList<Vehicle> vehs = vehicles.get(vehpacks.get(pack));
+		if(vehicle >= vehs.size()) vehicle = 0;
+		if(vehicle < 0) vehicle = vehs.size() - 1;
+		veh = vehs.get(vehicle);
+		texts.get("vehicle").value(veh.getName());
+		switchRecipe(0);
+	}
+
+	private void switchRecipe(int i){
+		data = new VehicleData(veh);
+		int slots = 0;
+		int prov = 0;
+		for(PartSlots ps : data.getPartSlotProviders().values()){
+			slots += ps.size();
+			prov++;
+		}
+		texts.get("desc0").translate("ui.fvtm.vehicle_catalog.desc0", data.getWheelPositions().size(), data.getSeats().size());
+		texts.get("desc1").translate("ui.fvtm.vehicle_catalog.desc1", prov, slots);
+		texts.get("desc2").translate("ui.fvtm.vehicle_catalog.desc2", recipe + 1, 1);
 	}
 
 }
