0173e9c6fa2332de0d498b2b70a14cf8c852d9bf
Ferdinand Calo <fexcalo@gmail.com>
2024-04-08 22:07:30 +0200

    Create VehicleConstructorCon.java

---
+++ src/net/fexcraft/mod/fvtm/ui/vehicle/VehicleConstructorCon.java
@@ -0,0 +1,17 @@
+package net.fexcraft.mod.fvtm.ui.vehicle;
+
+import net.fexcraft.app.json.JsonMap;
+import net.fexcraft.lib.common.math.V3I;
+import net.fexcraft.mod.uni.ui.ContainerInterface;
+import net.fexcraft.mod.uni.world.EntityW;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class VehicleConstructorCon extends ContainerInterface {
+
+	public VehicleConstructorCon(JsonMap map, EntityW ply, V3I pos){
+		super(map, ply, pos);
+	}
+
+}
