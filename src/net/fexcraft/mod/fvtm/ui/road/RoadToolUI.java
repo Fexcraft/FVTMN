7f2f947a764d359892313db5c571771d4e52c6d0
Ferdinand Calo <fexcalo@gmail.com>
2024-04-09 09:20:01 +0200

    Create RoadToolUI.java

---
+++ src/net/fexcraft/mod/fvtm/ui/road/RoadToolUI.java
@@ -0,0 +1,24 @@
+package net.fexcraft.mod.fvtm.ui.road;
+
+import net.fexcraft.app.json.JsonMap;
+import net.fexcraft.mod.fvtm.sys.uni.VehicleInstance;
+import net.fexcraft.mod.uni.ui.ContainerInterface;
+import net.fexcraft.mod.uni.ui.UIText;
+import net.fexcraft.mod.uni.ui.UserInterface;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class RoadToolUI extends UserInterface {
+
+	public RoadToolUI(JsonMap map, ContainerInterface con) throws Exception{
+		super(map, con);
+		//
+	}
+
+	@Override
+	public void predraw(float ticks, int mx, int my){
+		//
+	}
+
+}
