4466aa2cb20e6349299f3d4aa590576b26cb09a5
Ferdinand Calo <fexcalo@gmail.com>
2024-04-10 23:11:13 +0200

    Update RoadToolCon.java

--- src/net/fexcraft/mod/fvtm/ui/road/RoadToolCon.java
+++ src/net/fexcraft/mod/fvtm/ui/road/RoadToolCon.java
@@ -1,12 +1,7 @@
 package net.fexcraft.mod.fvtm.ui.road;
 
 import net.fexcraft.app.json.JsonMap;
-import net.fexcraft.lib.common.math.Time;
 import net.fexcraft.lib.common.math.V3I;
-import net.fexcraft.mod.fvtm.data.Fuel;
-import net.fexcraft.mod.fvtm.data.attribute.Attribute;
-import net.fexcraft.mod.fvtm.sys.uni.Passenger;
-import net.fexcraft.mod.fvtm.sys.uni.VehicleInstance;
 import net.fexcraft.mod.uni.item.StackWrapper;
 import net.fexcraft.mod.uni.tag.TagCW;
 import net.fexcraft.mod.uni.ui.InventoryInterface;
@@ -17,8 +12,27 @@ import net.fexcraft.mod.uni.world.EntityW;
  */
 public abstract class RoadToolCon extends InventoryInterface {
 
+	public static final String[] fills = new String[]{ "RoadFill", "BottomFill", "SideLeftFill", "SideRightFill", "TopFill", "LinesFill" };
+	protected StackWrapper stack;
+	protected boolean custom_road;
+	protected boolean custom_top;
+	protected boolean custom_lines;
+
 	public RoadToolCon(JsonMap map, EntityW player, V3I pos){
 		super(map, player, pos);
+		stack = player.getHeldItem(true);
+		stack.createTagIfMissing();
+		custom_road = stack.getTag().has("CustomRoadFill");
+		custom_top = stack.getTag().has("CustomTopFill");
+		custom_lines = stack.getTag().has("CustomLinesFill");
+	}
+
+	protected void initInv(){
+		for(int i = 0; i < fills.length; i++){
+        	if(stack.getTag().has(fills[i])){
+				setInventoryContent(i, stack.getTag().getCompound(fills[i]));
+        	}
+        }
 	}
 
 	@Override
@@ -29,12 +43,24 @@ public abstract class RoadToolCon extends InventoryInterface {
 
 	@Override
 	public void packet(TagCW com, boolean client){
-		//
+		switch(com.getString("cargo")){
+			case "save":{
+				stack.getTag().set("RoadLayers", com.getIntArray("sizes"));
+				break;
+			}
+		}
 	}
 
 	@Override
-	public void update(Object lc){
-		//
+	public void onClosed(){
+		for(int i = 0; i < fills.length; i++){
+        	if(!isInventoryEmpty(i)){
+				TagCW tag = TagCW.create();
+				getInventoryContent(i).save(tag);
+        		stack.getTag().set(fills[i], tag);
+        	}
+        	else stack.getTag().rem(fills[i]);
+        }
 	}
 
 }
