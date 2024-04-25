8faacac83760a0318c1ae60ed2741b4493acd1a9
Ferdinand Calo <fexcalo@gmail.com>
2024-04-26 00:24:22 +0200

    Update DefaultPrograms.java

--- src/net/fexcraft/mod/fvtm/model/program/DefaultPrograms.java
+++ src/net/fexcraft/mod/fvtm/model/program/DefaultPrograms.java
@@ -1,8 +1,19 @@
 package net.fexcraft.mod.fvtm.model.program;
 
+import net.fexcraft.lib.common.math.Time;
+import net.fexcraft.lib.mc.utils.Print;
+import net.fexcraft.mod.fvtm.FvtmLogger;
 import net.fexcraft.mod.fvtm.model.Program;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneOffset;
+import java.util.Date;
 import java.util.Timer;
+import java.util.TimerTask;
+
+import static net.fexcraft.mod.fvtm.Config.BLINKER_INTERVAL;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
@@ -14,6 +25,20 @@ public class DefaultPrograms {
 
 	public static void init(){
 		//
+	}
+
+	public static void setupBlinkerTimer(){
+		if(BLINKER_TIMER != null) BLINKER_TIMER.cancel();
+		FvtmLogger.debug("Setting up blinker-toggle timer.");
+		LocalDateTime midnight = LocalDateTime.of(LocalDate.now(ZoneOffset.systemDefault()), LocalTime.MIDNIGHT);
+		long mid = midnight.toInstant(ZoneOffset.UTC).toEpochMilli(); long date = Time.getDate();
+		while((mid += BLINKER_INTERVAL) < date);
+		(BLINKER_TIMER = new Timer()).schedule(new TimerTask(){
+			@Override
+			public void run(){
+				BLINKER_TOGGLE = !BLINKER_TOGGLE;
+			}
+		}, new Date(mid), BLINKER_INTERVAL);
 	}
 
 }
