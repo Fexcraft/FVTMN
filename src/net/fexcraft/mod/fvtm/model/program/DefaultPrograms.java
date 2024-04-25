44065c69f2759a808ba9249e716c6d9940ac96e7
Ferdinand Calo <fexcalo@gmail.com>
2024-04-26 00:21:44 +0200

    Create DefaultPrograms.java

---
+++ src/net/fexcraft/mod/fvtm/model/program/DefaultPrograms.java
@@ -0,0 +1,19 @@
+package net.fexcraft.mod.fvtm.model.program;
+
+import net.fexcraft.mod.fvtm.model.Program;
+
+import java.util.Timer;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class DefaultPrograms {
+
+	public static boolean BLINKER_TOGGLE;
+	public static Timer BLINKER_TIMER;
+
+	public static void init(){
+		//
+	}
+
+}
