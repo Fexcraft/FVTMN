fa47cb83aa89aab828ff80d360218dcc24b301ca
Ferdinand Calo <fexcalo@gmail.com>
2024-04-28 13:02:01 +0200

    Update VehicleData.java

--- src/net/fexcraft/mod/fvtm/data/vehicle/VehicleData.java
+++ src/net/fexcraft/mod/fvtm/data/vehicle/VehicleData.java
@@ -745,8 +745,4 @@ public class VehicleData extends ContentData<Vehicle, VehicleData> implements Co
 		return parts.get(origin.split("\\|")[0]);
 	}
 
-	public boolean isBraking(){
-		return false;//TODO
-	}
-
 }
