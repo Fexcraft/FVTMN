346d0317f0f13a117ed299e68ebbe0acbdcc1f2b
Ferdinand Calo <fexcalo@gmail.com>
2024-05-02 22:44:26 +0200

    Update DebugUtils.java

--- src/net/fexcraft/mod/fvtm/util/DebugUtils.java
+++ src/net/fexcraft/mod/fvtm/util/DebugUtils.java
@@ -1,7 +1,14 @@
 package net.fexcraft.mod.fvtm.util;
 
+import net.fexcraft.lib.frl.Polyhedron;
+import net.fexcraft.lib.tmt.ModelRendererTurbo;
+
 public class DebugUtils {
 
 	public static boolean ACTIVE = false;
+	//
+	public static Polyhedron SPHERE = new Polyhedron()
+		.importMRT(new ModelRendererTurbo(null, 0, 0, 1, 1)
+			.addSphere(0, 0, 0, 1, 16, 16, 1, 1), false, 1f);
 
 }
