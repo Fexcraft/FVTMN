20c5f0ee6b91ef5f18a7cad6b247d346abad2951
Ferdinand Calo <fexcalo@gmail.com>
2024-05-12 12:11:45 +0200

    Update SeatsFunction.java

--- src/net/fexcraft/mod/fvtm/function/part/SeatsFunction.java
+++ src/net/fexcraft/mod/fvtm/function/part/SeatsFunction.java
@@ -6,37 +6,33 @@ import java.util.Map.Entry;
 
 import net.fexcraft.app.json.FJson;
 import net.fexcraft.app.json.JsonValue;
+import net.fexcraft.lib.common.math.V3D;
 import net.fexcraft.mod.fvtm.data.Seat;
 import net.fexcraft.mod.fvtm.data.part.Part;
 import net.fexcraft.mod.fvtm.data.part.PartFunction;
 import net.fexcraft.mod.fvtm.data.part.PartFunction.StaticFunction;
+import net.fexcraft.mod.fvtm.util.ContentConfigUtil;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
 public class SeatsFunction extends StaticFunction {
-	
-	private ArrayList<Seat> seats = new ArrayList<>();
+
+	private V3D offset = V3D.NULL;
 
 	@Override
 	public PartFunction init(Part part, FJson json){
-		for(Entry<String, JsonValue<?>> entry : json.asMap().entries()){
-			seats.add(new Seat(entry.getKey(), entry.getValue().asMap()));
-		}
+		offset = ContentConfigUtil.getVector(json.asArray(), 0);
 		return this;
 	}
 
 	@Override
 	public String getId(){
-		return "fvtm:seats";
-	}
-	
-	public Collection<Seat> getPositions(){
-		return seats;
+		return "fvtm:connector";
 	}
 
-	public ArrayList<Seat> getSeats(){
-		return seats;
+	public V3D getOffset(){
+		return offset;
 	}
-	
+
 }
