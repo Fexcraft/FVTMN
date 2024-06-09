ac08b8e313208ac84f4563e090faff3e8332586f
Ferdinand Calo <fexcalo@gmail.com>
2024-06-09 10:06:56 +0200

    Create ItemDataCache.java

---
+++ src/net/fexcraft/mod/fvtm/util/ItemDataCache.java
@@ -0,0 +1,31 @@
+package net.fexcraft.mod.fvtm.util;
+
+import net.fexcraft.mod.fvtm.data.ContentData;
+import net.fexcraft.mod.fvtm.data.vehicle.VehicleData;
+import net.fexcraft.mod.uni.item.StackWrapper;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class ItemDataCache<CN extends ContentData<?, ?>> {
+
+	protected StackWrapper wrapper;
+	protected CN content;
+
+	public CN getContent(){
+		return content;
+	}
+
+	public StackWrapper getWrapper(){
+		return wrapper;
+	}
+
+	public static class VehicleDataCache extends ItemDataCache<VehicleData> {
+
+		public VehicleDataCache(StackWrapper stack){
+			wrapper = stack;
+		}
+
+	}
+
+}
