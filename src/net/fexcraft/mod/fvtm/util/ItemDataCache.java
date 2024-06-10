bea31bc266ba11587ee1571dcfdfb7c6176e96b2
Ferdinand Calo <fexcalo@gmail.com>
2024-06-10 16:17:06 +0200

    Update ItemDataCache.java

--- src/net/fexcraft/mod/fvtm/util/ItemDataCache.java
+++ src/net/fexcraft/mod/fvtm/util/ItemDataCache.java
@@ -1,6 +1,7 @@
 package net.fexcraft.mod.fvtm.util;
 
 import net.fexcraft.mod.fvtm.data.ContentData;
+import net.fexcraft.mod.fvtm.data.part.PartData;
 import net.fexcraft.mod.fvtm.data.vehicle.VehicleData;
 import net.fexcraft.mod.uni.item.StackWrapper;
 
@@ -23,6 +24,14 @@ public class ItemDataCache<CN extends ContentData<?, ?>> {
 	public static class VehicleDataCache extends ItemDataCache<VehicleData> {
 
 		public VehicleDataCache(StackWrapper stack){
+			wrapper = stack;
+		}
+
+	}
+
+	public static class PartDataCache extends ItemDataCache<PartData> {
+
+		public PartDataCache(StackWrapper stack){
 			wrapper = stack;
 		}
 
