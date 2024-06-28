47413aff017ebad4976ff52044384caf7b872e32
Ferdinand Calo <fexcalo@gmail.com>
2024-06-28 23:03:48 +0200

    Update VehicleMainCon.java

--- src/net/fexcraft/mod/fvtm/ui/vehicle/VehicleMainCon.java
+++ src/net/fexcraft/mod/fvtm/ui/vehicle/VehicleMainCon.java
@@ -3,11 +3,11 @@ package net.fexcraft.mod.fvtm.ui.vehicle;
 import net.fexcraft.app.json.JsonMap;
 import net.fexcraft.lib.common.math.V3I;
 import net.fexcraft.mod.fvtm.sys.uni.Passenger;
-import net.fexcraft.mod.fvtm.ui.UIKey;
+import net.fexcraft.mod.fvtm.ui.UIKeys;
 import net.fexcraft.mod.uni.UniEntity;
 import net.fexcraft.mod.uni.tag.TagCW;
 import net.fexcraft.mod.uni.ui.ContainerInterface;
-import net.fexcraft.mod.uni.world.EntityW;
+import net.fexcraft.mod.uni.ui.UIKey;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
@@ -28,12 +28,12 @@ public class VehicleMainCon extends ContainerInterface {
 		if(client) return;
 		UIKey ui = null;
 		switch(com.getString("open")){
-			case "info": ui  = UIKey.VEHICLE_INFO; break;
-			case "fuel": ui  = UIKey.VEHICLE_FUEL; break;
-			case "attributes": ui  = UIKey.VEHICLE_ATTRIBUTES; break;
-			case "inventories": ui  = UIKey.VEHICLE_INVENTORIES; break;
-			case "containers": ui  = UIKey.VEHICLE_CONTAINERS; break;
-			case "connectors": ui  = UIKey.VEHICLE_CONNECTORS; break;
+			case "info": ui  = UIKeys.VEHICLE_INFO; break;
+			case "fuel": ui  = UIKeys.VEHICLE_FUEL; break;
+			case "attributes": ui  = UIKeys.VEHICLE_ATTRIBUTES; break;
+			case "inventories": ui  = UIKeys.VEHICLE_INVENTORIES; break;
+			case "containers": ui  = UIKeys.VEHICLE_CONTAINERS; break;
+			case "connectors": ui  = UIKeys.VEHICLE_CONNECTORS; break;
 			default: return;
 		}
 		if(ui != null) ((Passenger)player).openUI(ui, pos);
