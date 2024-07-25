c8661fdfb120a1103af6960a5140feca5614707c
Ferdinand Calo <fexcalo@gmail.com>
2024-07-25 14:51:20 +0200

    Update LiftingPoint.java

--- src/net/fexcraft/mod/fvtm/data/vehicle/LiftingPoint.java
+++ src/net/fexcraft/mod/fvtm/data/vehicle/LiftingPoint.java
@@ -1,6 +1,7 @@
 package net.fexcraft.mod.fvtm.data.vehicle;
 
 import net.fexcraft.app.json.JsonArray;
+import net.fexcraft.app.json.JsonMap;
 import net.fexcraft.lib.common.math.V3D;
 import net.fexcraft.mod.fvtm.util.ContentConfigUtil;
 
@@ -18,6 +19,13 @@ public class LiftingPoint {
 		pos = ContentConfigUtil.getVector(array);
 		second = array.size() > 3 ? array.get(3).string_value() : null;
 		off = array.size() > 4 ? array.get(4).float_value() : 0;
+	}
+
+	public LiftingPoint(String key, JsonMap map){
+		id = key;
+		pos = ContentConfigUtil.getVector(map.getArray("pos"));
+		second = map.getString("pair", null);
+		off = map.getFloat("offset", 0);
 	}
 	
 	public LiftingPoint(String key, V3D pos2, String str, float yoff0){
