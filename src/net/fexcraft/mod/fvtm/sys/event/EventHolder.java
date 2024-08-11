22789118f7a8487e951d765058661871c5cefc25
Ferdinand Calo <fexcalo@gmail.com>
2024-08-11 10:58:43 +0200

    Update EventHolder.java

--- src/net/fexcraft/mod/fvtm/sys/event/EventHolder.java
+++ src/net/fexcraft/mod/fvtm/sys/event/EventHolder.java
@@ -1,10 +1,13 @@
 package net.fexcraft.mod.fvtm.sys.event;
 
+import net.fexcraft.mod.fvtm.data.root.Soundable;
+import net.fexcraft.mod.fvtm.data.root.Soundable.SoundHolder;
 import net.fexcraft.mod.fvtm.sys.uni.Passenger;
 import net.fexcraft.mod.fvtm.sys.uni.VehicleInstance;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 /**
@@ -12,23 +15,39 @@ import java.util.Map;
  */
 public class EventHolder {
 
-	public HashMap<EventType, ArrayList<EventListener>> listeners = new HashMap<>();
+	public HashMap<EventType, ArrayList<EventListener>> listeners = new LinkedHashMap<>();
+	public HashMap<String, EventHolder> subholders = new LinkedHashMap<>();
+	public SoundHolder sounds;
+	public Object root;
 
-	public void integrate(EventHolder holder){
-		for(Map.Entry<EventType, ArrayList<EventListener>> entry : holder.listeners.entrySet()){
-			if(!listeners.containsKey(entry.getKey())) listeners.put(entry.getKey(), new ArrayList<>());
-			listeners.get(entry.getKey()).addAll(entry.getValue());
-		}
+	public EventHolder(Object root){
+		this.root = root;
+		sounds = (SoundHolder)root;
 	}
 
-	public void deintegrate(EventHolder holder){
-		ArrayList<EventListener> lis;
-		for(Map.Entry<EventType, ArrayList<EventListener>> entry : holder.listeners.entrySet()){
-			lis = listeners.get(entry.getKey());
-			if(lis == null) continue;
-			lis.removeAll(entry.getValue());
+	public void integrate(EventHolder holder, String origin){
+		if(origin == null){
+			for(Map.Entry<EventType, ArrayList<EventListener>> entry : holder.listeners.entrySet()){
+				if(!listeners.containsKey(entry.getKey())) listeners.put(entry.getKey(), new ArrayList<>());
+				listeners.get(entry.getKey()).addAll(entry.getValue());
+			}
 		}
-		listeners.entrySet().removeIf(entry -> entry.getValue().isEmpty());
+		else subholders.put(origin, holder);
+	}
+
+	public void deintegrate(EventHolder holder, String origin){
+		if(origin == null){
+			ArrayList<EventListener> lis;
+			for(Map.Entry<EventType, ArrayList<EventListener>> entry : holder.listeners.entrySet()){
+				lis = listeners.get(entry.getKey());
+				if(lis == null) continue;
+				lis.removeAll(entry.getValue());
+			}
+			listeners.entrySet().removeIf(entry -> entry.getValue().isEmpty());
+		}
+		else{
+			if(subholders.containsKey(origin)) subholders.remove(origin);
+		}
 	}
 
 	public void insert(EventListener lis){
@@ -37,9 +56,16 @@ public class EventHolder {
 	}
 
 	public void run(EventType event, VehicleInstance inst, Passenger pass, Object... args){
-		if(!listeners.containsKey(event)) return;
-		for(EventListener lis : listeners.get(event)){
-			lis.action.run(EventAction.DATA.set(inst, pass), lis);
+		if(listeners.containsKey(event)){
+			for(EventListener lis : listeners.get(event)){
+				lis.action.run(EventAction.DATA.set(inst, pass, this, this), lis, args);
+			}
+		}
+		for(EventHolder hol : subholders.values()){
+			if(!hol.listeners.containsKey(event)) continue;
+			for(EventListener lis : hol.listeners.get(event)){
+				lis.action.run(EventAction.DATA.set(inst, pass, this, hol), lis, args);
+			}
 		}
 	}
 
